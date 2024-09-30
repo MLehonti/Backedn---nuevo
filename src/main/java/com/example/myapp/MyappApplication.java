525eeecb2d093046e8ae37ebaaa3af9b023a073b
MLGM2001 <canonmauricio63@gmail.com>
2024-09-30 19:38:59 -0400

    agregar la creacion de updoloads

--- src/main/java/com/example/myapp/MyappApplication.java
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -1,3 +1,36 @@
+//package com.example.myapp;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@SpringBootApplication
+//public class MyappApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(MyappApplication.class, args);
+//	}
+//	@Configuration
+//	public static class Myconfiguration{
+//		@Bean
+//		public WebMvcConfigurer corsConfigurer(){
+//			return new WebMvcConfigurer() {
+//
+//				@Override
+//				public void addCorsMappings(CorsRegistry registry){
+//					registry.addMapping("/**")
+//					 .allowedMethods("HEAD", "GET", "PUT","POST","DELETE","PATCH" );
+//				}
+//			};
+//		}
+//	}
+
+//}
+
+
 package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
@@ -7,25 +40,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
+
 @SpringBootApplication
 public class MyappApplication {
 
 	public static void main(String[] args) {
+		// Crear el directorio 'uploads' si no existe
+		File uploadsDir = new File("uploads");
+		if (!uploadsDir.exists()) {
+			uploadsDir.mkdir(); // Crear el directorio 'uploads'
+			System.out.println("Directorio 'uploads' creado con éxito.");
+		} else {
+			System.out.println("Directorio 'uploads' ya existe.");
+		}
+
 		SpringApplication.run(MyappApplication.class, args);
 	}
+
 	@Configuration
-	public static class Myconfiguration{
+	public static class Myconfiguration {
+
 		@Bean
-		public WebMvcConfigurer corsConfigurer(){
+		public WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurer() {
 
 				@Override
-				public void addCorsMappings(CorsRegistry registry){
+				public void addCorsMappings(CorsRegistry registry) {
 					registry.addMapping("/**")
-					 .allowedMethods("HEAD", "GET", "PUT","POST","DELETE","PATCH" );
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 				}
 			};
 		}
 	}
-
 }
+
